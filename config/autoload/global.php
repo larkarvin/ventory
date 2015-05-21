d761938244e06783c7ae2aff654b5c825c306593
arvin almario <arvin@almar.io>
2015-05-21 23:29:13 +0800

    changing password

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -26,7 +26,7 @@ return array(
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
-        'username' => 'root',
-        'password' => 'password',
+        'username' => 'zakaph',
+        'password' => 'pwd@zak@',
     ),
 );
